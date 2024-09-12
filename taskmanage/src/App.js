ca12674db8c3682712940b2a6939057d9aad56be
Jacob Jeffords <jacobjeffords64@gmail.com>
2024-09-12 18:38:42 -0400

    React Routing implemented

--- taskmanage/src/App.js
+++ taskmanage/src/App.js
@@ -1,17 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
+import Login from './components/Login';
 import Main from './components/Main';
 import Footer from './components/Footer';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
+    <Router>
       <Header />
-      <Main />
+
+      <Routes>
+        <Route path="/" element={<Main />} />
+
+        <Route path="/login" element={<Login />} />
+
+        <Route path="/main" element={<Main />} />
+      </Routes>
+
       <Footer />
-    </div>
+    </Router>
   );
-}
+};
 
 export default App;
