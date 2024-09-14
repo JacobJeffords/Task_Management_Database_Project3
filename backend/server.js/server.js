3a19910237d8c0e8622356af6acdbbeaeec3b722
“richardcrn3rd” <richardcrn3rd@gmail.com>
2024-09-14 14:32:58 -0400

    Added Routes

--- backend/server.js/server.js
+++ backend/server.js/server.js
@@ -1,18 +1,26 @@
 require('dotenv').config();
 const express = require('express');
 const session = require('express-session');
-const mongoose = require('mongoose');
+const pgSession = require('connect-pg-simple')(session);
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const userRoutes = require('./routes/users');
-const taskRoutes = require('./routes/tasks');
+const pool = require('./db');
+const userRoutes = require('./routes/userRoutes');
+const taskRoutes = require('./routes/tasksRoutes');
+
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(cors());
+
+//Session management with PostgreSQL
 app.use(session({
+    store: new pgSession({
+        pool,
+        tableName:'session'
+    }),
     secret: process.env.SESSION_SECRET || 'your_session_secret',
     resave: false,
     saveUninitialized: false
@@ -21,9 +29,6 @@ app.use(session({
 app.use('/users', userRoutes);
 app.use('/tasks', taskRoutes);
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.error('Failed to connect to MongoDB', err));
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
