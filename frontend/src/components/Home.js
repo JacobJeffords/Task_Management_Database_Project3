831ec79da9fd853291b3036eb7e0455a81400b21
Jacob Jeffords <jacobjeffords64@gmail.com>
2024-09-21 10:59:19 -0400

    Further implementation of form reading

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -2,41 +2,70 @@ import React, { useState, useRef } from 'react';
 import './index.css';
 
 function Main() {
-  function taskWindowRef = useRef(null);
-  function OpenTaskWindow() { document.getElementById('taskWindow').showModal() }
-  function CloseTaskWindow() { document.getElementById('taskWindow').close() }
-  function [taskName, setTaskName] = useState('');
-  function [taskDetails, setTaskDetails] = useState('');
-  function [taskAssignees, setTaskAssignees] = useState('');
-  function [taskDate, setTaskDate] = useState('');
-  function [taskTime, setTaskTime] = useState('');
-  function [tasks, setTasks] = useState([]);
+  const taskWindowRef = useRef(null);
+  const [taskName, setTaskName] = useState('');
+  const [taskDetails, setTaskDetails] = useState('');
+  const [taskAssignees, setTaskAssignees] = useState('');
+  const [taskDate, setTaskDate] = useState('');
+  const [taskTime, setTaskTime] = useState('');
+  const [taskDueDate, setTaskDueDate] = useState('');
+  const [taskDueTime, setTaskDueTime] = useState('');
+  const [tasks, setTasks] = useState([]);
+
+  function OpenTaskWindow() {
+    document.getElementById('taskWindow').showModal();
+  }
+
+  function CloseTaskWindow() {
+    document.getElementById('taskWindow').close();
+  }
+
+  function handleSubmitTask() {
+    const newTask = {
+      name: taskName,
+      details: taskDetails,
+      assignees: taskAssignees,
+      date: taskDate,
+      time: taskTime,
+    };
+
+    setTasks([...tasks, newTask]);
+    setTaskName('');
+    setTaskDetails('');
+    setTaskAssignees('');
+    setTaskDate('');
+    setTaskTime('');
+    setTaskDueDate('');
+    setTaskDueTime('');
+
+    CloseTaskWindow();
+  }
 
   return (
     <div id="mainContainer">
       <main>
-
         <div id="taskStorage">
           <div id="taskStorageHeader">
             <h2>Task Panel</h2>
           </div>
           <div id="taskStoragePanel">
-
+            { }
           </div>
         </div>
-        
-        <main>
+
+        <div>
           <button id="createTask" onClick={OpenTaskWindow}>
             Create New Task
           </button>
 
           <dialog id="taskWindow" ref={taskWindowRef}>
             <h1>Enter Your Task</h1>
+
             <p>Task Name:</p>
             <input
               type="text"
               value={taskName}
-              onChange={(e) => setTaskName(e.target.value)} //event listener or handler react version
+              onChange={(e) => setTaskName(e.target.value)}
               placeholder="Enter task name"
             />
 
@@ -68,11 +97,22 @@ function Main() {
               onChange={(e) => setTaskTime(e.target.value)}
             />
 
-            <br></br>
+            <p>Task Due Date:</p>
+            <input
+              type="date"
+              value={taskDueDate}
+              onChange={(e) => setTaskDueDate(e.target.value)}
+            />
+            <input
+              type="time"
+              value={taskDueTime}
+              onChange={(e) => setTaskDueTime(e.target.value)}
+            />
+
+            <br />
             <button onClick={handleSubmitTask}>Submit Task</button>
           </dialog>
-          <button onClick={CloseTaskWindow}>Submit Task</button>
-        </dialog>
+        </div>
       </main>
     </div>
   );
