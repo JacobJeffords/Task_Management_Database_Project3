5583357c7b32d81dc6b2581e48d0a51d42df8bdb
“richardcrn3rd” <richardcrn3rd@gmail.com>
2024-09-26 18:18:47 -0400

    Corrected Error in Index.js

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const userRoutes = require('./routes/users');
 const taskRoutes = require('./routes/tasks');
-
+const path = require('path');
 const cors = require('cors');
 const app = express();
 
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 app.use(cors())
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, 'client', 'build')));
+  app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
 app.use('/users', userRoutes);
